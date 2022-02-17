831b35e8233654f113fee838df77500ce29da09a
Gonzalo Sanchez Garay <gmsanchezgaray@gmail.com>
2022-02-17 18:13:54 -0300

    Add a color background to the container

--- src/components/styles/Global.js
+++ src/components/styles/Global.js
@@ -32,6 +32,7 @@ export const Container = styled.div`
   height: 33.5rem;
   border-radius: 8px;
   max-width: 100%;
+  background-color: #efefef;
   padding: 2.5rem 0px;
   box-shadow: rgba(17, 17, 26, 0.1) 0px 4px 16px,
     rgba(17, 17, 26, 0.05) 0px 8px 32px;
